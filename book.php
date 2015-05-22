2380fa02218814bf22aec1470bc76751cee696eb
Markus Thulin <macky_b@hotmail.com>
2015-05-22 15:08:36 +0200

    Fixed coding standard error in book.php

--- book.php
+++ book.php
@@ -45,7 +45,8 @@ if (!isset($_SESSION['sess_user'])) {
 <div class="navbar navbar-inverse navbar-fixed-top">
 <div class="container">
 <div class="navbar-header">
-<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+<button type="button" class="navbar-toggle"
+        data-toggle="collapse" data-target=".navbar-collapse">
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
