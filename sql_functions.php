2c18644c595f49d422b530ee3f112d220521b79c
Markus Thulin <macky_b@hotmail.com>
2015-12-11 12:45:53 +0100

    Added functions for the 2 top ten lists

--- sql_functions.php
+++ sql_functions.php
@@ -12,6 +12,25 @@
 */
 
     /**
+    * PrintTable
+    *
+    * @param string $head1 The First header
+    * @param string $head2 The Second header
+    * @param array $data The Array containg the data to be printed
+    *
+    * @return void
+    */
+function printTable($head1, $head2, $data)
+{
+    echo '<table class="table table-striped">';
+    echo '<thead><tr><th>' . $head1 . '</th><th>' . $head2 . '</th></tr></thead><tbody>';
+    foreach ($data as $key => $value) {
+        echo '<tr><td>' . $value[0] . '</td><td>' . $value[1] . '</td></tr>';
+    }
+    echo '</tbody></table>';
+}
+
+    /**
     * ShowStatsForSeason
     *
     * @param string $season The season you want to get stats for
@@ -24,17 +43,51 @@ function showStatsForSeason($season)
     $mysqli->set_charset('utf8');
 
     $query  = sprintf(
-        'SELECT `name`, `%s` FROM `stats` WHERE `%s`!=0 ORDER BY `%s` DESC', 
-        $mysqli->real_escape_string($season), $mysqli->real_escape_string($season), 
+        'SELECT `name`, `%s` FROM `stats` WHERE `%s`!=0 ORDER BY `%s` DESC',
+        $mysqli->real_escape_string($season), $mysqli->real_escape_string($season),
         $mysqli->real_escape_string($season)
     );
     $result = $mysqli->query($query);
     $row    = $result->fetch_all(MYSQLI_NUM);
 
-    echo '<table class="table table-striped">';
-    echo '<thead><tr><th>Namn</th><th>Antal</th></tr></thead><tbody>';
-    foreach ($row as $key => $value) {
-        echo '<tr><td>' . $value[0] . '</td><td>' . $value[1] . '</td></tr>';
-    }
-    echo '</tbody></table>';  
+    printTable("Namn", "Antal", $row);
+}
+
+    /**
+    * ShowStatsTopTenTotal
+    *
+    * @return void
+    */
+function showStatsTopTenTotal()
+{
+    include 'connect.php';
+    $mysqli->set_charset('utf8');
+
+    $result = $mysqli->query(
+        'SELECT `name`, `aut2012`+`spring2013`+`aut2013`+`spring2014`+`aut2014`+
+        `spring2015`+`aut2015` FROM `stats` ORDER BY `aut2012`+`spring2013`+
+        `aut2013`+`spring2014`+`aut2014`+`spring2015`+`aut2015` DESC LIMIT 10'
+    );
+    $row    = $result->fetch_all(MYSQLI_NUM);
+
+    printTable("Namn", "Totalt", $row);
+}
+
+    /**
+    * ShowStatsTopTenSeasons
+    *
+    * @return void
+    */
+function showStatsTopTenSeasons()
+{
+    include 'connect.php';
+    $mysqli->set_charset('utf8');
+
+    $result = $mysqli->query(
+        'SELECT `name`, `nbr_seasons` FROM `stats` ORDER BY `nbr_seasons`
+        DESC LIMIT 10'
+    );
+    $row    = $result->fetch_all(MYSQLI_NUM);
+
+    printTable("Namn", "Antal S&auml;songer", $row);
 }
