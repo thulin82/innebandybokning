0b5111edbbbae1a8beb42f1c16930e5dff20872d
Markus Thulin <macky_b@hotmail.com>
2015-12-18 10:48:22 +0100

    Updated jQuery to 1.11.3 (should be used in bootstrap v3.3.6)

--- about.php
+++ about.php
@@ -94,7 +94,7 @@ if ($sess_id == 2) {
 
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">
 </script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
