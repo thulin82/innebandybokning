48e1b375acc74bc18302695df456fad419d9c33a
Markus Thulin <macky_b@hotmail.com>
2026-08-12 10:58:55 +0200

    test setup

---
+++ test/config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Config
+ *
+ * Get all configuration details to be able to execute the test suite.
+ *
+ * PHP version 7
+ *
+ * @category Config
+ * @package  Innebandybokning
+ * @author   Markus Thulin <macky_b@hotmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT
+ * @link     https://github.com/thulin82/innebandybokning
+ */
+define('DB_HOST', 'db');
+define('DB_USER', 'db_user');
+define('DB_PASS', 'my_pw');
+define('DB_NAME', 'my_db');
+
+require __DIR__ . '/../vendor/autoload.php';
